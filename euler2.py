5135e51ea27bf4ff67a6351aef2607d7df6682b4
Bryan <brysmyth90@googlemail.com>
2014-09-09 23:24:57 +0100

    Finished problem 2.

--- euler2.py
+++ euler2.py
@@ -1,4 +1,12 @@
+"""By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+Answer:
+	4613732"""
+tot=2
 fib=[1,2]
 while fib[-1]<=4000000:
-	fib.append(sum(fib[-2:-1]))
-print "Answer is %d"%sum([n for n in fib if n%2==0])
+	#fib.append(sum(fib[-2:-1]))
+	fib=[fib[-1]]+[sum(fib)]
+	if fib[-1]%2==0:
+		tot+=fib[-1]
+print "Answer is %d"%tot
